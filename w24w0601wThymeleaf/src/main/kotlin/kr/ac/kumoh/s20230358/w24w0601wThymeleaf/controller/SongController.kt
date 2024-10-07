00b4d847482529424897455d2d1f136472f4f6a3
hyobin77 <gyqls0509@kumoh.ac.kr>
2024-10-07 16:44:23 +0900

    model추가

--- w24w0601wThymeleaf/src/main/kotlin/kr/ac/kumoh/s20230358/w24w0601wThymeleaf/controller/SongController.kt
+++ w24w0601wThymeleaf/src/main/kotlin/kr/ac/kumoh/s20230358/w24w0601wThymeleaf/controller/SongController.kt
@@ -1,12 +1,14 @@
 package kr.ac.kumoh.s20230358.w24w0601wThymeleaf.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class SongController {
     @GetMapping("/song/random")
-    fun  getRandomSong(): String{
+    fun  getRandomSong(model: Model): String{
+        model.addAttribute("songTitle","테스형")
         return "random"
     }
 
